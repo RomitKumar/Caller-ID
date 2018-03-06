c4d42432456d18cddfa503af2e475525a4ff2f0d
Ankit Kumar <AKJAIS99@GMAIL.COM>
2018-03-06 21:17:13 +0530

    Create caller_id.py

---
+++ caller_id.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup as bsoup
+from requests import post,get
+
+def main():
+    url='https://mbasic.facebook.com/login/identify/?ctx=recover&search_attempts=1'
+    data = {'email': input('Enter e-Mail or Phone: ')}
+    a = post(url,data = data)
+    r = bsoup(a.content,'lxml')
+    temp = r.findAll('strong')
+    res = []
+    for i in temp:
+        c = i.contents[0]
+        if c.isnumeric():
+                continue
+        res.append(c)
+    res = '\n'.join(res)
+    print(res if res else 'Not Found')
+
+if __name__ == '__main__':
+    main()
