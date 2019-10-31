a010fca3fd6415bad4a569e10d0295408bc4b012
romit111 <57224274+romit111@users.noreply.github.com>
2019-10-31 19:26:01 +0530

    Update caller_id.py

--- caller_id.py
+++ caller_id.py
@@ -5,6 +5,9 @@ def main():
     url='https://mbasic.facebook.com/login/identify/?ctx=recover&search_attempts=1'
     data = {'email': input('Enter e-Mail or Phone: ')}
     a = post(url,data = data,headers = {'Cookie':'locale=en_GB'})
+    if not a.ok:
+        print('Error making connection... Try again')
+        return
     r = bsoup(a.content,'lxml')
     temp = r.findAll('strong')
     res = []
